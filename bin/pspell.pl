d1265fc954deb1954fea799b7d7839e14a32d8b7
Luciano Dante Cecere <ldante86@aol.com>
2017-11-12 02:40:44 -0800

    update

--- bin/pspell.pl
+++ bin/pspell.pl
@@ -8,12 +8,11 @@ use File::Basename qw(dirname);
 use Cwd qw(abs_path);
 use lib dirname( dirname abs_path $0) . '/lib';
 
-use Pspell;
-
 if ( "$ARGV[0]" eq "-h" ) {
     die "Usage: pspell.pl [-v] [-h] [word] [file]\n";
 }
 
+use Pspell;
 pspell_main(@ARGV);
 
 __END__
